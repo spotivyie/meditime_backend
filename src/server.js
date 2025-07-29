fc489b0ea1d53ce434d6ab758bccdaf0e0178f7a
Eduarda Cardoso <eduardac.brandao@hotmail.com>
2025-07-29 01:10:27 -0400

    atualizando projeto

--- src/server.js
+++ src/server.js
@@ -12,11 +12,25 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://meditime-frontend.vercel.app'
+];
+
 app.use(cookieParser());
+
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
+    origin: function(origin, callback) {
+        if (!origin) return callback(null, true); 
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'Acesso negado por CORS: origem não permitida.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true,
 }));
+
 app.use(express.json());
 
 app.use('/uploads', express.static('uploads'));
